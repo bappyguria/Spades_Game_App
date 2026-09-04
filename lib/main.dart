7bfafe547604b40b21efef1b6f91ca18c85dc1a0
bappyguria <guriabappye8@gmail.com>
2026-09-04 13:16:51 +0600

    App Logo and Name Change

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Spades Game",
+      title: "Real Spades Game",
       home: const HomeScreen(),
     );
   }
