c07177155c2b16587ef21db63660f971427cb5c2
bappyguria <guriabappye8@gmail.com>
2026-09-05 13:37:50 +0600

    pub change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-
 import 'firebase_options.dart';
-import 'screens/home_screen.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
